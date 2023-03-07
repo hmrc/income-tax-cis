5068e352210971cbf1469199a7ad1f3dd659026d
Hristo Georgiev <georgievh@yahoo.co.uk>
2023-03-07 09:09:58 +0000

    SASS-4607 - CIS for 2023-24 Update CIS Deduction Submission API#1791.

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -19,17 +19,17 @@ import sbt._
 
 object AppDependencies {
 
-  private val bootstrapPlay28Version = "7.13.0"
+  private val bootstrapPlay28Version = "7.14.0"
 
   val compile: Seq[ModuleID] = Seq(
     "uk.gov.hmrc"                   %%  "bootstrap-backend-play-28"  % bootstrapPlay28Version,
-    "com.fasterxml.jackson.module"  %%  "jackson-module-scala"       % "2.14.1"
+    "com.fasterxml.jackson.module"  %%  "jackson-module-scala"       % "2.14.2"
   )
 
   val test: Seq[ModuleID] = Seq(
     "uk.gov.hmrc"             %% "bootstrap-test-play-28"   % bootstrapPlay28Version    % Test,
     "com.typesafe.play"       %% "play-test"                % current                   % Test,
-    "org.scalatest"           %% "scalatest"                % "3.2.14"                  % Test,
+    "org.scalatest"           %% "scalatest"                % "3.2.15"                  % Test,
     "com.vladsch.flexmark"    %  "flexmark-all"             % "0.62.2"                  % "test, it",
     "org.scalatestplus.play"  %% "scalatestplus-play"       % "5.1.0"                   % "test, it",
     "com.github.tomakehurst"  %  "wiremock-jre8"            % "2.35.0"                  % "test, it",
